cbcbba247d7f87910ba428f1da547889358f2d81
KleoPetroff <kleopetroff@gmail.com>
2015-12-12 10:10:05 +0200

    rename course folders

--- Javascript-Basic/JavaScript-Development-Introduction/1. Current-Time/assets/javascript/script.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-var getTime = function () {
-    return new Date().toUTCString()
-};
-
-var element = document.getElementById('currentTime');
-element.innerHTML = getTime();
--- Javascript-Basic/JavaScript-Development-Introduction/3. Decimal to Hexadecimal/assets/javascript/dec2hex.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var getDecimal = prompt('Enter a decimal number');
-
-var decToHex = function (dec) {
-    return (+dec).toString(16).toUpperCase();
-};
-
-alert(decToHex(getDecimal));
