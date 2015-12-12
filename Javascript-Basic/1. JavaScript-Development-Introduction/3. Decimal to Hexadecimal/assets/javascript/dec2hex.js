f19d0e55c5e41d4de8bf2b1d788c35c38fae3376
KleoPetroff <kleopetroff@gmail.com>
2015-12-12 10:11:22 +0200

    add renamed Introduction Javascript Folder

---
+++ Javascript-Basic/1. JavaScript-Development-Introduction/3. Decimal to Hexadecimal/assets/javascript/dec2hex.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var getDecimal = prompt('Enter a decimal number');
+
+var decToHex = function (dec) {
+    return (+dec).toString(16).toUpperCase();
+};
+
+alert(decToHex(getDecimal));
