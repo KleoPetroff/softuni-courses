50ffb08e210138ff5b98ae93f6c5701b6b52ca39
KleoPetroff <kleopetroff@gmail.com>
2015-12-12 10:29:48 +0200

    add km/h to Knot solution

---
+++ Javascript-Basic/2. JavaScript Syntax/Calculate Knots/assets/javascript/knots.js
@@ -0,0 +1,16 @@
+(function() {
+    'use strict';
+
+    var calculateButton = document.getElementById('calculate'),
+        result = document.getElementById('result'),
+        kmHValue = document.getElementById('kilometersPerHour');
+
+    var kmHtoKnots = function (kmH) {
+        return (parseFloat(kmH) / 1.852).toFixed(2)
+    };
+
+    calculateButton.addEventListener('click', function () {
+        result.innerHTML = kmHtoKnots(kmHValue.value);
+    })
+
+}());
