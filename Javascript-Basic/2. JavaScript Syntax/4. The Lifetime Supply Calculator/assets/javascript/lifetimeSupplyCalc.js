b66e46f649cf3131703482f1c05c55ebfa03cf58
KleoPetroff <kleopetroff@gmail.com>
2015-12-14 09:07:22 +0200

    add Lifetime Supply Calculator solution

---
+++ Javascript-Basic/2. JavaScript Syntax/4. The Lifetime Supply Calculator/assets/javascript/lifetimeSupplyCalc.js
@@ -0,0 +1,25 @@
+(function() {
+    'use strict';
+
+    var submitButton = document.getElementById('calculate'),
+        result = document.getElementById('result');
+
+    var calcSupply = function (age, maxAge, food, foodPerDay) {
+        return  ((maxAge - age) * foodPerDay) * 365 + 'kg of ' + food +
+            ' will be enough until I am ' + maxAge + ' years old.';
+    };
+
+    submitButton.addEventListener('click', function () {
+
+        var inputs = {
+            age: parseFloat(document.getElementById('age').value),
+            maxAge: parseFloat(document.getElementById('maxAge').value),
+            food: document.getElementById('favoriteFood').value,
+            foodPerDay: parseFloat(document.getElementById('foodPerDay').value)
+        };
+
+        result.innerHTML = calcSupply(inputs.age, inputs.maxAge, inputs.food, inputs.foodPerDay);
+        document.getElementById('form').reset();
+    });
+
+}());
