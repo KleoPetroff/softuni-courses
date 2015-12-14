627828a6894ee571cd0f6a4058d8a7d883a7ed43
KleoPetroff <kleopetroff@gmail.com>
2015-12-14 21:17:29 +0200

    add Calculate Expression solution
    Don't ever use eval() in production code.. EVER!!

---
+++ Javascript-Basic/2. JavaScript Syntax/5. Calculate Expression/assets/javascript/calcExpression.js
@@ -0,0 +1,12 @@
+(function() {
+    'use strict';
+
+    var submitButton = document.getElementById('evalButton'),
+        result = document.getElementById('result');
+
+    submitButton.addEventListener('click', function () {
+        var expression = document.getElementById('expression');
+
+        result.innerHTML = eval(expression.value); // DON'T EVER, EVER USE eval()
+    })
+}());
