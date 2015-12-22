841c20588c63d45f4fe26a96232c744ee3a9083b
KleoPetroff <kleopetroff@gmail.com>
2015-12-22 23:04:47 +0200

    improve readability by removing whitespaces

--- Javascript-Basic/5. Javascript DOM/2. Email Only Field/assets/javascript/emailValidator.js
+++ Javascript-Basic/5. Javascript DOM/2. Email Only Field/assets/javascript/emailValidator.js
@@ -8,13 +8,12 @@
     };
 
     var validateEmail = function (emailStr) {
-
         var resultElement = inputs.validation;
+
         resultElement.innerHTML = inputs.emailInput.value;
         if (emailStr.match(/@\S/g) && emailStr.match(/\.\S/)) resultElement.style.background = '#76ee00';
         else resultElement.style.background = '#ff0000';
     };
-
 
     inputs.submit.addEventListener('click', function () {
         validateEmail(inputs.emailInput.value);
