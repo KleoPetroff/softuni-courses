bb23b6cd7f0a4817c6c97af0fd8a0401d6b73159
KleoPetroff <kleopetroff@gmail.com>
2015-12-23 20:11:19 +0200

    add Function Arguments solution

---
+++ Advanced-Javascript/1. Functions and Function Expressions/1. Function Arguments/argumentsInfo.js
@@ -0,0 +1,60 @@
+(function () {
+    'use strict';
+
+    var printArgsInfo = (function() {
+
+        var flatten = function (array) {
+            var ret = [],
+                i;
+
+            for(i = 0; i < array.length; i++) {
+                if(Array.isArray(array[i])) {
+                    ret = ret.concat(flatten(array[i]));
+                } else {
+                    ret.push(array[i]);
+                }
+            }
+
+            return ret;
+        };
+
+        var checkType = function (element) {
+            var type =  Function.prototype.toString.call(element.constructor);
+
+            return type.match(/function (.*)\(/)[1].toLocaleLowerCase();
+        };
+
+        var getType = function () {
+            var args = Array.prototype.slice.call(arguments);
+
+            return args.map(function (arg) {
+                return arg && typeof args === 'object' ? checkType(arg) : typeof arg;
+            });
+        };
+
+        var result = function () {
+            var args =  Array.prototype.slice.call(arguments);
+            var result = '';
+
+            args.forEach(function (element) {
+               if (getType(element)[0] === 'array') {
+                   result += flatten(element) + ' (' + getType(element)[0] + ')\n';
+               } else {
+                   result += element + ' (' + getType(element)[0] + ')\n';
+               }
+            });
+
+            return result;
+        };
+
+        return {
+            check: result
+        }
+    }());
+
+    console.log(printArgsInfo.check(null, undefined, "", 0, [], {}));
+    console.log(printArgsInfo.check(2, 3, 2.5, -110.5564, false));
+    console.log(printArgsInfo.check([1, 2], ["string", "array"], ["single value"]));
+    console.log(printArgsInfo.check("some string", [1, 2], ["string", "array"], ["mixed", 2, false, "array"], {name: "Peter", age: 20}));
+    console.log(printArgsInfo.check([[1, [2, [3, [4, 5]]]], ["string", "array"]]));
+}());
