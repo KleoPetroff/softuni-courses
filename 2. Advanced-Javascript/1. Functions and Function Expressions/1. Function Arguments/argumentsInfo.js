8255ec3c421ca7f7a9fe01928f5263329b858b9a
KleoPetroff <kleopetroff@gmail.com>
2015-12-27 18:35:37 +0200

    refactor code to remove ESLint errors

--- 2. Advanced-Javascript/1. Functions and Function Expressions/1. Function Arguments/argumentsInfo.js
+++ 2. Advanced-Javascript/1. Functions and Function Expressions/1. Function Arguments/argumentsInfo.js
@@ -2,13 +2,12 @@
     'use strict';
 
     var printArgsInfo = (function() {
-
         var flatten = function (array) {
             var ret = [],
                 i;
 
-            for(i = 0; i < array.length; i++) {
-                if(Array.isArray(array[i])) {
+            for (i = 0; i < array.length; i++) {
+                if (Array.isArray(array[i])) {
                     ret = ret.concat(flatten(array[i]));
                 } else {
                     ret.push(array[i]);
@@ -19,38 +18,39 @@
         };
 
         var checkType = function (element) {
-            var type =  Function.prototype.toString.call(element.constructor);
+            var type = Function.prototype.toString.call(element.constructor);
 
             return type.match(/function (.*)\(/)[1].toLocaleLowerCase();
         };
 
         var getType = function (element) {
-             return element === 'object' ? checkType(element) : typeof element;
+            return element === 'object' ? checkType(element) : typeof element;
         };
 
         var result = function () {
-            var args =  Array.prototype.slice.call(arguments);
-            var result = '';
+            var args = Array.prototype.slice.call(arguments);
+            var final = '';
 
             args.forEach(function (element) {
-               if (getType(element) === 'array') {
-                   result += flatten(element) + ' (' + getType(element) + ')\n';
-               } else {
-                   result += element + ' (' + getType(element) + ')\n';
-               }
+                if (getType(element) === 'array') {
+                    final += flatten(element) + ' (' + getType(element) + ')\n';
+                } else {
+                    final += element + ' (' + getType(element) + ')\n';
+                }
             });
 
-            return result;
+            return final;
         };
 
         return {
             check: result
-        }
+        };
     }());
 
-    console.log(printArgsInfo.check(null, undefined, "", 0, [], {}));
+
+    console.log(printArgsInfo.check(null, undefined, '', 0, [], {}));
     console.log(printArgsInfo.check(2, 3, 2.5, -110.5564, false));
-    console.log(printArgsInfo.check([1, 2], ["string", "array"], ["single value"]));
-    console.log(printArgsInfo.check("some string", [1, 2], ["string", "array"], ["mixed", 2, false, "array"], {name: "Peter", age: 20}));
-    console.log(printArgsInfo.check([[1, [2, [3, [4, 5]]]], ["string", "array"]]));
+    console.log(printArgsInfo.check([1, 2], ['string', 'array'], ['single value']));
+    console.log(printArgsInfo.check('some string', [1, 2], ['string', 'array'], ['mixed', 2, false, 'array'], { name: 'Peter', age: 20 }));
+    console.log(printArgsInfo.check([[1, [2, [3, [4, 5]]]], ['string', 'array']]));
 }());
