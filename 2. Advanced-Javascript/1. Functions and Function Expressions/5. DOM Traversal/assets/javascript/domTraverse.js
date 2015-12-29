1480973958d52aabbf389ebddc4e5627f8b3b6b0
KleoPetroff <kleopetroff@gmail.com>
2015-12-29 22:08:13 +0200

    isolate the code in iife

--- 2. Advanced-Javascript/1. Functions and Function Expressions/5. DOM Traversal/assets/javascript/domTraverse.js
+++ 2. Advanced-Javascript/1. Functions and Function Expressions/5. DOM Traversal/assets/javascript/domTraverse.js
@@ -1,26 +1,28 @@
-'use strict';
+(function() {
+    'use strict';
 
-function traverse(selector) {
-    var node = document.querySelector(selector);
-    function traverseNode(node, spacing) {
+    var traverse = function (selector) {
+        var node = document.querySelector(selector);
 
-        var nodeId = node.getAttribute('id'),
-            nodeClass = node.getAttribute('class');
-        spacing = spacing || '  ';
-        console.log(spacing + node.nodeName.toLowerCase() + ':'
-            + (nodeId ? ' id="' + nodeId + '"' : '')
-            + (nodeClass ? ' class="' + nodeClass + '"' : ''));
+        var traverseNode = function(node, spacing) {
+            var nodeId = node.getAttribute('id'),
+                nodeClass = node.getAttribute('class');
+            spacing = spacing || '  ';
+            console.log(spacing + node.nodeName.toLowerCase() + ':'
+                + (nodeId ? ' id="' + nodeId + '"' : '')
+                + (nodeClass ? ' class="' + nodeClass + '"' : ''));
 
-        for (var i = 0; i < node.childNodes.length; i++) {
-            var child = node.childNodes[i];
-            if (child.nodeType === document.ELEMENT_NODE) {
-                traverseNode(child, spacing + '  ');
+            for (var i = 0; i < node.childNodes.length; i++) {
+                var child = node.childNodes[i];
+                if (child.nodeType === document.ELEMENT_NODE) {
+                    traverseNode(child, spacing + '  ');
+                }
             }
+        };
+        if (node !== undefined && node !== null) {
+            traverseNode(node, '');
         }
-    }
-    if (node !== undefined && node !== null) {
-        traverseNode(node, '');
-    }
-}
+    };
 
-traverse('.birds');
+    traverse('.birds');
+}());
