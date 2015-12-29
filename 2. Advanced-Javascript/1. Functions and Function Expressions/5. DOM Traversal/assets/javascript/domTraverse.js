b9e1042dd4d978bdca2b8e331b1418286e7759b6
KleoPetroff <kleopetroff@gmail.com>
2015-12-29 22:02:56 +0200

    add dom traverse solution

---
+++ 2. Advanced-Javascript/1. Functions and Function Expressions/5. DOM Traversal/assets/javascript/domTraverse.js
@@ -0,0 +1,26 @@
+'use strict';
+
+function traverse(selector) {
+    var node = document.querySelector(selector);
+    function traverseNode(node, spacing) {
+
+        var nodeId = node.getAttribute('id'),
+            nodeClass = node.getAttribute('class');
+        spacing = spacing || '  ';
+        console.log(spacing + node.nodeName.toLowerCase() + ':'
+            + (nodeId ? ' id="' + nodeId + '"' : '')
+            + (nodeClass ? ' class="' + nodeClass + '"' : ''));
+
+        for (var i = 0; i < node.childNodes.length; i++) {
+            var child = node.childNodes[i];
+            if (child.nodeType === document.ELEMENT_NODE) {
+                traverseNode(child, spacing + '  ');
+            }
+        }
+    }
+    if (node !== undefined && node !== null) {
+        traverseNode(node, '');
+    }
+}
+
+traverse('.birds');
